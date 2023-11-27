6747c47bb6740d10cebaa65bc4552eeec144ccff
LuisinhoGameplays87 <151741504+LuisinhoGameplays87@users.noreply.github.com>
2023-11-27 11:36:11 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("navbar").style.width = "100%";
+    document.getElementById("navbar").style.height = "100%";
+}
+
+/* Close */
+function closeNavUp() {
+  document.getElementById("navbar").style.height = "0%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("navbar").style.width = "0%";
+}
+
